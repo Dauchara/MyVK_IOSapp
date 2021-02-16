1d3b42c63479e91801836ec598b8a8a80feb30f8
Dauchara <pro100_gold_man@mail.ru>
2021-02-16 23:53:07 +0600

    search bar finished

--- MyVk_IOSapp/TableControllers/FriendsTableViewController.swift
+++ MyVk_IOSapp/TableControllers/FriendsTableViewController.swift
@@ -9,14 +9,31 @@ import UIKit
 
 class FriendsTableViewController: UITableViewController {
 
-    let userList = User()
     
+    @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var friendsTableView: UITableView!
+    
+    let userList = User()
+    var searchedFriends = [UserModel]()
+    var searching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         friendsTableView.register(UINib(nibName: "FriendsViewCell", bundle: nil), forCellReuseIdentifier: "friendCell")
+        
+        // сохраним для истории))
+//        let searchTextField = self.searchBar.searchTextField
+//                searchTextField.textColor = UIColor.white
+//                searchTextField.clearButtonMode = .never
+//                searchTextField.backgroundColor = UIColor.black
+//        
+//        let glassIconView = searchTextField.leftView as! UIImageView
+//                glassIconView.image = glassIconView.image?.withRenderingMode(.alwaysTemplate)
+//                glassIconView.tintColor = UIColor.red
+        
+        self.searchBar.showsCancelButton = true
+        self.searchBar.delegate = self
     }
 
     // MARK: - Table view data source
@@ -28,14 +45,23 @@ class FriendsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return self.userList.users.count
+        if searching {
+            return self.searchedFriends.count
+        } else {
+            return self.userList.users.count
+        }
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath) as! FriendsViewCell
-        let user = userList.users[indexPath.row]
-        cell.setupCell(user: user)
+        if searching {
+            let user = self.searchedFriends[indexPath.row]
+            cell.setupCell(user: user)
+        } else {
+            let user = userList.users[indexPath.row]
+            cell.setupCell(user: user)
+        }
         return cell
     }
     
@@ -54,4 +80,18 @@ class FriendsTableViewController: UITableViewController {
         return UIView()
     }
 
+}
+
+extension FriendsTableViewController : UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchedFriends = userList.users.filter { $0.sName.lowercased().prefix(searchText.count) == searchText.lowercased() }
+        searching = true
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        searchBar.text = ""
+        tableView.reloadData()
+    }
 }
